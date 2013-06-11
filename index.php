1a3f80c50033d9d61a4370b16648556f2ccffa84
Daniel Boorn <daniel.boorn@gmail.com>
2013-06-11 19:01:08 -0400

    Comments Update
    Comments Update

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 	 * @author Daniel Boorn - daniel.boorn@gmail.com
 	 * @copyright Daniel Boorn
 	 * @license Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
-	 * @requires phpQuery
-	 * @namespace TheCity
 	 */
 
 	require_once('vendor/com.rapiddigitalllc/thecity/admin.php');
